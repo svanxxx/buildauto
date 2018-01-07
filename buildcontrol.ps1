966629923087c17617b646f761653d6d0c2c1e72
svanxxx <6204931@gmail.com>
2018-01-07 13:31:32 +0300

    temp workout

--- buildcontrol.ps1
+++ buildcontrol.ps1
@@ -1,9 +1,30 @@
-write-host "Starting build machines control"
-start-sleep -seconds 5
+# extract current file path
+$thispath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+# include WOL command
+. ($thispath + '.\SendWOL.ps1')
+
+echo "Starting build machines control"
 Do
 {
+	echo "Sleep 2 seconds"
+    start-sleep -seconds 2
+    # here is the code to get the xml file
+    # ...
+    # end
 
-	write-host "Sleep 2 seconds"
-	start-sleep -seconds 2  
+    $file1 = "$($thispath)\tmp\BuildCoordinatorStatus.xml"
+    $file2 = "$($thispath)\tmp\BuildCoordinatorStatusNormalized.xml"
+    #originally source file is a huge single line file. first: converting it to multiline
+    (gc $($file1)) -replace '><', ">`r`n<" | Out-File $($file2)
 
+    $dowwakeup = Select-String -Path $file2 -Pattern "Building=""False"""
+    if ($dowwakeup -ne $null)
+    {
+        echo Contains String
+    }
+    else
+    {
+        echo Not Contains String
+    }    
+	#Send-WOL -mac 001132212D11 
 } While ($TRUE)
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 run next command as administrator to enable running scripts in your system:
 
 Set-ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy bypass
