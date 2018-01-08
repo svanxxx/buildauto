1d490e53d4d2ee6577fe835dbd77518ada550ff1
svanxxx <6204931@gmail.com>
2018-01-08 20:12:46 +0300

    first approach

---
+++ GetMACAddress.ps1
@@ -0,0 +1,7 @@
+function Get-MACAddress { 
+    param ($strComputer) 
+    $colItems = get-wmiobject -class "Win32_NetworkAdapterConfiguration" -computername $strComputer |Where{$_.IpEnabled -Match "True"}  
+    foreach ($objItem in $colItems) {  
+        $objItem |select MACAddress  
+    } 
+} 
--- buildcontrol.ps1
+++ buildcontrol.ps1
@@ -2,29 +2,76 @@
 $thispath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 # include WOL command
 . ($thispath + '.\SendWOL.ps1')
+. ($thispath + '.\GetMACAddress.ps1')
+
+$statusfile = "$($thispath)/tmp/Status.xml"
 
 echo "Starting build machines control"
 Do
 {
-	echo "Sleep 2 seconds"
-    start-sleep -seconds 2
     # here is the code to get the xml file
-    # ...
-    # end
+    $Command = "xgcoordconsole.exe"
+    $Parms = ("/exportstatus=$($statusfile)")
+    $Prms = $Parms.Split(" ")
+    & "$Command" $Prms
 
-    $file1 = "$($thispath)\tmp\BuildCoordinatorStatus.xml"
-    $file2 = "$($thispath)\tmp\BuildCoordinatorStatusNormalized.xml"
-    #originally source file is a huge single line file. first: converting it to multiline
-    (gc $($file1)) -replace '><', ">`r`n<" | Out-File $($file2)
 
-    $dowwakeup = Select-String -Path $file2 -Pattern "Building=""False"""
-    if ($dowwakeup -ne $null)
+    [xml]$XmlDocument = Get-Content -Path $statusfile
+    $Agents = $XmlDocument.SelectSingleNode("//Agents") 
+    $Building = $false
+    FOREACH ($Agent in $Agents.ChildNodes)
     {
-        echo Contains String
+        if ($Agent.Attributes.GetNamedItem("Building").Value -eq "True")
+        {
+           $Building = $true
+           break
+        }
+    }
+    if ($Building -eq $false)
+    {
+        echo "stopping machines..."
+        FOREACH ($Agent in $Agents.ChildNodes)
+        {
+            $AgentName = $Agent.Attributes.GetNamedItem("Host").Value
+            if ($Agent.Attributes.GetNamedItem("Online").Value -eq "True")
+            {
+                $machinefilename = "$($thispath)\machines\$($AgentName)"
+                $results = Get-MACAddress $AgentName | Out-String
+                if ($results -ne "")
+                {
+                    $results | Out-File $machinefilename
+                }
+            }
+            if (($Agent.Attributes.GetNamedItem("LoggedOnUsers").Value -eq "") -and ($Agent.Attributes.GetNamedItem("Online").Value -eq "True"))
+            {
+                echo $AgentName
+                stop-computer $AgentName
+            }
+        }
     }
     else
     {
-        echo Not Contains String
-    }    
-	#Send-WOL -mac 001132212D11 
+       echo "starting machines..."
+       FOREACH ($Agent in $Agents.ChildNodes)
+       {
+           $AgentName = $Agent.Attributes.GetNamedItem("Host").Value
+           $AgentFile = "$($thispath)/machines/$($AgentName)"
+           if ((Test-Path $AgentFile) -and ($Agent.Attributes.GetNamedItem("LoggedOnUsers").Value -eq "") -and ($Agent.Attributes.GetNamedItem("Online").Value -eq "False"))
+           {
+                foreach($line in Get-Content $AgentFile)
+                {
+                    if($line.Contains(":"))
+                    {
+                       $line = $line -replace ':',''
+                       $Command = "$($thispath)/bin/WolCmd.exe"
+                       $Parms = "$($line) 192.168.0.1 255.255.255.0 3"
+                       $Prms = $Parms.Split(" ")
+                       & "$Command" $Prms
+                    }
+                }
+           }
+       }
+    }
+    echo "Sleep 15 seconds"
+    start-sleep -seconds 15
 } While ($TRUE)
