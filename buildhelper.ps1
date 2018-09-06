5b22e8076fc74ec25488a286977ac9333d402925
svanxxx <6204931@gmail.com>
2018-09-06 15:35:39 +0300

    build helper iteration cleanup

--- buildhelper.ps1
+++ buildhelper.ps1
@@ -5,47 +5,58 @@ $bstfile = "$($workdir)Common\BSTUserName.h"
 $temp = [System.IO.Path]::GetTempPath();
 $outfile = "$($temp)buildoutput.log";
 $fipoutfile = "$($temp)fipbuildoutput.log";
+#$svc = New-WebServiceProxy –Uri ‘http://192.168.0.1/taskmanagerbeta/trservice.asmx?WSDL’
+$svc = New-WebServiceProxy –Uri ‘http://localhost:8311/TRService.asmx?WSDL’
+$request = $null;
 
-function Build-Version($buildrequest)
+function Progress-Out([string]$txt)
 {
-    $branch = "TT$($buildrequest.TTID)";
-    $user = "$($buildrequest.USER)";
-    $version = "V8E";
-    $ttid = "$($branch) $($buildrequest.SUMMARY)";
-    $comment = "$($buildrequest.COMM)";
+    $txt | Out-File $($outfile) -Append;
+    $svc.CommentBuild($request.ID, $txt);
+    echo $txt;
+}
 
-    "Starting build..." | Out-File $($outfile)
-    "$($ttid)" | Out-File $($outfile) -Append;
+function Build-Version()
+{
+    $branch = "TT$($request.TTID)";
+    $user = "$($request.USER)";
+    $version = "V8E";
+    $ttid = "$($branch) $($request.SUMMARY)";
+    $comment = "$($request.COMM)";
+
+    "buildheler:" | Out-File $($outfile);
+    Progress-Out "Starting build..."
+    Progress-Out "$($ttid)"
 
     #=========================================================
     # GIT - getting code
     #=========================================================
-    "getting code from git..." | Out-File $($outfile) -Append;
-    $branch | Out-File $($outfile) -Append;
+    Progress-Out "getting code from git..."
+    Progress-Out $branch
     Set-Location $($workdir);
     cmd /c "git reset --hard" | Out-File $($outfile) -Append;
     cmd /c "git fetch --all" | Out-File $($outfile) -Append;
     cmd /c "git checkout $($branch)" | Out-File $($outfile) -Append;
     cmd /c "git pull origin" | Out-File $($outfile) -Append;
 
-    return
-
     #=========================================================
     # FIP - building
     #=========================================================
-    "building fieldpro..." | Out-File $($outfile) -Append;
+    Progress-Out "building fieldpro..."
+
+    return
 
     "#define _BSTUserName _T("".$($user)"")" | Out-File $($bstfile)
 
     $buildcommand = "BuildConsole.exe ""$($workdir)Projects.32\All.sln"" /rebuild /cfg=""Release|Mixed Platforms"" /NOLOGO /OUT=""$($fipoutfile)"""
-    $buildcommand | Out-File $($outfile) -Append;
+    Progress-Out $buildcommand
 
     cmd /c "$($buildcommand)"
 
     #=========================================================
     # CX - building
     #=========================================================
-    "building CX..." | Out-File $($outfile) -Append;
+    Progress-Out "building CX..."
     Set-Location $($builddir);
     cmd /c """C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"" /clean Release Modules.sln"
 
@@ -63,19 +74,17 @@ function Build-Version($buildrequest)
     #=========================================================
     # CX - building
     #=========================================================
-    "Sending test request..." | Out-File $($outfile) -Append;
+    Progress-Out "Sending test request..."
     Set-Location $($testdir);
     cmd /c "RELEASE_TEST.BAT $($user) $($version) $($ttid) $($comment)" | Out-File $($outfile) -Append;
 }
 
-$svc = New-WebServiceProxy –Uri ‘http://192.168.0.1/taskmanagerbeta/trservice.asmx?WSDL’
-#$svc = New-WebServiceProxy –Uri ‘http://localhost:8311/TRService.asmx?WSDL’
 while ($true)
 {
     $request = $svc.getBuildRequest($env:computername.ToUpper())
     if (!($request.TTID -eq ""))
     {
-        Build-Version($request)
+        Build-Version
     }
     echo "$(Get-Date)"
     Start-Sleep -s 20
