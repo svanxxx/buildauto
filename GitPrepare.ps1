847c8db37e691a5905d02833ccf264be22d98d6c
svanxxx <6204931@gmail.com>
2020-01-29 21:54:17 +0300

    final update for git maintenance and startup

--- GitPrepare.ps1
+++ GitPrepare.ps1
@@ -2,6 +2,7 @@ while (-not (test-connection 192.168.0.1 -quiet)){}
 
 Set-Location c:\buildauto\
 git.exe reset --hard
+git.exe checkout master
 git.exe pull
 git.exe gc
 
@@ -10,5 +11,3 @@ git.exe reset --hard
 git.exe checkout master
 git.exe pull
 git.exe gc
-
-& "c:\buildauto\buildhelper.ps1"
