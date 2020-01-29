2171b4a09ebef76c9cf46b2f453380f69928609f
svanxxx <6204931@gmail.com>
2020-01-29 20:17:05 +0300

    new startup organization

---
+++ GitPrepare.ps1
@@ -0,0 +1,12 @@
+while (-not (test-connection 192.168.0.1 -quiet)){}
+
+Set-Location c:\buildauto\
+git.exe reset --hard
+git.exe pull
+git.exe gc
+
+Set-Location y:
+git.exe reset --hard
+git.exe checkout master
+git.exe pull
+git.exe gc
