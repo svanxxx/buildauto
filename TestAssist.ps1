9b8667779883a6cc31148e0043f6562a4586ff87
bst_master <bst@resnet.com>
2022-05-25 18:52:17 +0300

    assist fixes for md5

--- TestAssist.ps1
+++ TestAssist.ps1
@@ -28,32 +28,60 @@ while ($true) {
         $status = "$($directory)\status.txt"
         $request = "$($directory)\request.txt"
         $CheckFile = "$($directory)\processed.txt"
+        $LogFile = "$($directory)\log.txt"
         if (!(Test-Path -Path $CheckFile) -and (Test-File-Valid -Path $status) -and (Test-File-Valid -Path $request)) {
+            Add-Content -Path $LogFile -Value "$(Get-Date): new diretory found..."
             Write-Host "Detected goal directory: $($directory)"
             $Files = Get-Content -Path $status
             if ($Files -is [array]) {
                 $StartTest = $true
+                $hashes = [System.Collections.ArrayList]::new()
                 for ($f = 0; $f -lt $Files.Length; $f++) {
                     $FileNameToTest = Split-Path $Files[$f] -leaf
                     $file = "$($directory)\$($FileNameToTest)"
                     if (!(Test-File-Valid -Path $file)) {
                         $StartTest = $false
+                        Add-Content -Path $LogFile -Value "$(Get-Date): file is not ready: $($file)"
+                        break
+                    } else {
+                        $md5 = $file + ".md5"
+                        $ExpectedHash = Get-Content -Path $md5
+                        $hashes.Add($ExpectedHash)
                     }
                 }
                 if ($StartTest) {
                     Write-Host "All files are ready, processing..."
-                    "Processed time: $(Get-Date)" | Out-File -FilePath $CheckFile -Encoding ascii
+                    Add-Content -Path $LogFile -Value "$(Get-Date): Starting...."
                     $testGuid = Split-Path $directory -leaf
-                    New-Item -Path "$($WorkDirectory)$($testGuid)" -ItemType Directory
+                    New-Item -Path "$($WorkDirectory)$($testGuid)" -ItemType Directory -ErrorAction Ignore
+                    Remove-Item "$($WorkDirectory)$($testGuid)\*" -Recurse -Force
+                    $AllFilesAreOK = $true
                     for ($f = 0; $f -lt $Files.Length; $f++) {
                         $FileNameToTest = Split-Path $Files[$f] -leaf
                         $file = "$($directory)\$($FileNameToTest)"
                         $Destination = "$($WorkDirectory)$($testGuid)\$($FileNameToTest)"
+                        Add-Content -Path $LogFile -Value "$(Get-Date): Copy $($FileNameToTest)"
                         Copy-Item -Path $file -Destination $Destination
+                        Add-Content -Path $LogFile -Value "$(Get-Date): $($FileNameToTest) done"
+                        $hash = Get-FileHash $Destination -Algorithm MD5
+                        $hash = $hash.Hash
+                        if ($hash -ne $hashes[$f]){
+                            Write-Host "after file copy mD5 is wrong - repeating operation!"
+                            Add-Content -Path $LogFile -Value "$(Get-Date): MD5 is wrong!"
+                            $AllFilesAreOK = $false
+                            break
+                        } else {
+                            Add-Content -Path $LogFile -Value "$(Get-Date): MD5 is OK"
+                        }
                     }
-                    $params = Get-Content -Path $($request)
-                    $url = "http://localhost/api/request/add?" + $params.ToString()
-                    Invoke-RestMethod -Uri $($url)
+                    if ($AllFilesAreOK){
+                        "Processed time: $(Get-Date)" | Out-File -FilePath $CheckFile -Encoding ascii
+                        $params = Get-Content -Path $($request)
+                        $url = "http://localhost/api/request/add?" + $params.ToString()
+                        Add-Content -Path $LogFile -Value "$(Get-Date): Adding web request..."
+                        Invoke-RestMethod -Uri $($url)
+                        Add-Content -Path $LogFile -Value "$(Get-Date): web request done"
+                    }
                 }
             }
         }
